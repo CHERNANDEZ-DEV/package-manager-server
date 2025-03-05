397a02b1c7b8aec9c2abdee042bd7f4c6a615095
CARLOS HERNANDEZ <00002721@uca.edu.sv>
2025-03-05 16:29:57 -0600

    🚀 feat(order): implement service and controller
    Added service and controller layers for order management.
    Includes endpoints for creating, and retrieving orders.
    Business logic is handled in the service layer to ensure separation of concerns.

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -4,13 +4,24 @@ import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrdersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async createOrder(data: CreateOrderDto) {
-    return this.prisma.order.create({ data });
+    return this.prisma.order.create({
+      data: {
+        ...data,
+        packages: {
+          create: data.packages,
+        }
+      },
+      include: { packages: true },
+    })
   }
 
   async getAllOrders() {
-    return this.prisma.order.findMany();
+    return this.prisma.order.findMany({
+      include: { packages: true },
+    });
   }
+
 }
