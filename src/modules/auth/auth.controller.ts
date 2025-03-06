3c551894d435c91852b684081d1a640fdcd7b1e1
CARLOS HERNANDEZ <00002721@uca.edu.sv>
2025-03-05 18:27:23 -0600

    :construction: wip(auth): implement login y register methods for auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService, private usersService: UsersService) {}
+
+    @Post('register')
+    async register(@Body() userDto: { name: string; email: string; password: string }){
+        return this.usersService.createUser(userDto);
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: { email: string; password: string }){
+        return this.authService.login(loginDto);
+    }
+
+}
